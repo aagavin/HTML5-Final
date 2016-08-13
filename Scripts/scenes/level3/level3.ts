1ce28e71ffe1111f4f7e4cefe47d93c424ea204b
Aaron <gitlab@aagavin.ca>
2016-08-13 19:54:50 -0400

    added skeleton code for level3

--- Scripts/scenes/level3/level3.ts
+++ Scripts/scenes/level3/level3.ts
@@ -1,5 +1,47 @@
 module scenes{
 	export class Level3 extends objects.Scene{
-		
+		private _bgImage:createjs.Bitmap;
+
+
+		/**
+		 * Creates an instance of Level3.
+		 * 
+		 */
+		constructor(){
+			super();
+		}
+
+		//***************** public methods *****************// 
+
+		/**
+		 * Start method
+		 * 
+		 * @returns voids
+		 */
+		public Start():void{
+
+		}
+
+		/**
+		 * Update method
+		 * 
+		 * @returns voids
+		 */
+		public Update():void{
+
+		}
+
+		//***************** private methods *****************//
+
+		/**
+		 * check bounds of elements
+		 * 
+		 * @private
+		 * @returns void
+		 */
+		private checkBounds():void {
+			
+		}
+
 	}
 }
